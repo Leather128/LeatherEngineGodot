4c328699b72e9a2a9e18a841cfc3b06f0607651c
Leather128 <81660872+Leather128@users.noreply.github.com>
2022-05-30 16:09:52 -0400

    Update Title Screen.gd

--- Scenes/Title Screen.gd
+++ Scenes/Title Screen.gd
@@ -1,13 +1,18 @@
 extends Node2D
 
+var started: bool = false
+
 func _ready():
 	yield(get_tree().create_timer(0.1), "timeout")
 
 	Conductor.change_bpm(102)
 	AudioHandler.play_audio("Title Music")
+	
+	started = true
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_back"):
 		get_tree().quit()
 	
-	Conductor.songPosition += delta * 1000
+	if started:
+		Conductor.songPosition += delta * 1000
