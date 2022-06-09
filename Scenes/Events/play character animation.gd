bbb4162602156efdc3f49dc5fb3ce1b8055cb918
Leather128 <81660872+Leather128@users.noreply.github.com>
2022-06-08 23:21:14 -0400

    Update play character animation.gd

--- Scenes/Events/play character animation.gd
+++ Scenes/Events/play character animation.gd
@@ -1,4 +1,5 @@
 extends Event
 
 func process_event(argument_1, argument_2):
+	get_character_from_argument(argument_1).timer = 0
 	get_character_from_argument(argument_1).play_animation(argument_2, true)
