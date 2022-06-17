d4e0f417d2700744dfea857618797acdd21f3912
Leather128 <81660872+Leather128@users.noreply.github.com>
2022-06-17 16:25:35 -0400

    week 5?????

--- Scenes/Cutscenes/Cutscene.gd
+++ Scenes/Cutscenes/Cutscene.gd
@@ -1,6 +1,16 @@
 class_name Cutscene
 extends Node
 
+onready var game = $"../"
+
+onready var bf = game.bf
+onready var dad = game.dad
+onready var gf = game.gf
+
+onready var stage = game.stage
+
+onready var camera = game.get_node("Camera2D")
+
 signal finished
 
 func _ready():
