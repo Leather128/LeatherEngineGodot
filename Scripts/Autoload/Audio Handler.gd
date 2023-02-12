115ee9b0c63d033b3d04820f483cf82f2dd76579
Improjects <62845931+Squidtito@users.noreply.github.com>
2023-02-11 22:28:46 -0500

    Pitch changing in Freeplay as well when changing song speed

--- Scripts/Autoload/Audio Handler.gd
+++ Scripts/Autoload/Audio Handler.gd
@@ -5,10 +5,15 @@ onready var hitsound = $Hitsound
 func _ready():
 	hitsound.stream = load("res://Assets/Sounds/Hitsounds/" + Settings.get_data("hitsound") + ".ogg")
 
-func play_audio(audioName, startTime = 0.0):
+func play_audio(audioName, startTime = 0.0,speed = 1):
 	if get_node(audioName) != null:
+		get_node(audioName).pitch_scale = speed
 		get_node(audioName).play(startTime)
 
 func stop_audio(audioName):
 	if get_node(audioName) != null:
 		get_node(audioName).stop()
+		
+func get_audio_playback(audioName):
+	if get_node(audioName) != null:
+		return get_node(audioName).get_playback_position()
